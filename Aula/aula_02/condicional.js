bf296b7b797354d340835088838450559bc351c2
jose.ccna <jose.ccna@gmail.com>
2024-08-31 09:00:39 -0300

    inicio da aula 02

---
+++ Aula/aula_02/condicional.js
@@ -0,0 +1,41 @@
+// var numero = 1
+// switch(numero){
+//     case 0:
+//         console.log('caso 0')
+//     case 1:
+//         console.log('caso 1')
+//         break
+//     case 2:
+//         console.log('caso 2')
+// }
+
+
+//Dia da semana com swicht case.
+
+var dia = 7
+
+switch(dia){
+    case 1:
+        console.log('Segunda-feira')
+        break
+    case 2:
+        console.log('Terça-feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    case 7:
+        console.log('Domingo')
+        break
+    default:
+        console.log('Numero inválido')
+}
