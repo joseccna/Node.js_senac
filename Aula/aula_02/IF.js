496c610f5f2a5d16602389a3d89f506c5a02765c
jose.ccna <jose.ccna@gmail.com>
2024-08-31 10:26:52 -0300

    volta do intervalo

---
+++ Aula/aula_02/IF.js
@@ -0,0 +1,15 @@
+// Estrura de if / else.
+
+
+var numero = 201
+
+var resultado = 0
+
+resultado = numero % 2
+
+if(resultado == 0){
+    console.log('O numero ', numero ,' é par.')
+}
+else{
+    console.log(`O numero ${numero} é impar.`)
+}
