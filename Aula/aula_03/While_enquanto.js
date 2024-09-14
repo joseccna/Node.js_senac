c495db15498a7acf2f0ab6068a0fa91aa9b6b80d
jose.ccna <jose.ccna@gmail.com>
2024-09-14 10:41:12 -0300

    segunda parte da aula

---
+++ Aula/aula_03/While_enquanto.js
@@ -0,0 +1,59 @@
+
+// let contador = 1
+
+// while(contador <= 100){
+//     console.log('Jose')
+//     contador++
+// }
+
+/////////////////////////////////////////
+
+//let i = 7
+
+// while(i >= 1){
+//     console.log(`numero: ${i}`)
+//     i--
+// }
+
+///////////////////////////////////////////
+
+// let i = 2
+// while(i <= 20){
+//     console.log(i)
+//     i+=2
+// }
+
+//////////////////////////
+// S1
+
+// let i = 20
+// let nu = 10
+// while(i >= -10){
+//     console.log(nu)
+//     i-=3
+//     nu-=3
+// }
+
+// let nu = 3
+// let i = 1
+// let resutado = 0
+
+//////////////////////////
+// S2
+
+// while(i >= 10){
+//     resutado = nu + i
+//     console.log(resutado)
+//     i++
+// }
+
+//////////////////////////
+// S5
+let i = 1
+let nu = 0
+while(i <= 20){
+    console.log(nu)
+    i++
+    nu = i % 4
+}
+//////////////////////////
---
+++ Aula/aula_03/for.js
@@ -0,0 +1,23 @@
+// let i
+
+// for(i=1;i<=10;i++){
+//     console.log(`numero: ${i}`)
+// }
+
+/////////////////////////////////////
+
+let i
+let nu1 = 0
+let nu2 = 1
+let nu3 = nu1 + nu2
+
+console.log(0)
+console.log(1)
+for(i=1;i<=13;i++){
+console.log(nu3)
+ nu1 = nu2
+ nu2 = nu3
+ nu3 = nu1 + nu2
+}
+
+
